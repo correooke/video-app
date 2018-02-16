2e8685f7783dafcec3b63311b40ed69f54530a41
HSNBDES33\eocariz <eocariz@hasar.com>
2018-02-16 16:29:35 -0300

    Agrego botón e imágen

--- components/Card.js
+++ components/Card.js
@@ -1,28 +1,73 @@
 import React from 'react';
 import { View, 
         Text, 
+        Image, 
+        TouchableHighlight,
         StyleSheet } from 'react-native';
 
 const Card = () => {
     // ejecución de función. 
+    const imageSource = {
+        uri: 'https://www.revistawelldone.cl/wp-content/uploads/2017/12/avengersinfinitywar-1494357736132_1280w.jpg'
+    };
+    const onPressButton = () => {
+
+    };
 
     return (
         <View style={styles.container}>
+
             <Text style={styles.title} >
                 CARD (EJEMPLO)!
             </Text>
+            <Image style={styles.mainImage} 
+                source={imageSource} />
+            <Text style={styles.description} >
+                Descripción de ejemplo de video
+                largo
+            </Text> 
+            <TouchableHighlight
+                style={styles.button}
+                underlayColor="#EEE"
+                onPress={onPressButton} >
+                <Text 
+                    style={styles.buttonText} >
+                    Ver más
+                </Text>
+            </TouchableHighlight>
         </View> 
     )
 };
 
 const styles = StyleSheet.create({
     container: {
-        height: 200,
+        height: 300,
         width: 300,
-        backgroundColor: '#F00',
+        backgroundColor: '#0F0',
     },
     title: {
         fontSize: 30
+    },
+    mainImage: {
+        height: 150,
+        width: 280,
+        marginLeft: 10, 
+    },
+    button: {
+        height: 30,
+        width: 280,
+        marginLeft: 10,
+        marginTop: 10,
+        backgroundColor: "#999",
+    }, 
+    buttonText: {
+        fontSize: 20,
+        color: "#222",
+    },
+    description: { 
+        marginLeft: 10,
+        fontSize: 13,
+        color: "#EEE",
     }
 })
 export default Card;
