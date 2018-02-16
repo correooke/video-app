cbda49b1277167619d2ba808d606a0e8c52600bf
HSNBDES33\eocariz <eocariz@hasar.com>
2018-02-16 11:30:52 -0300

    Subida inicial

---
+++ components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Card = () => {
+    // ejecución de función. 
+
+    return (
+        <View>
+            <Text>
+                CARD (EJEMPLO)!
+            </Text>
+        </View> 
+    )
+};
+
+export default Card;
