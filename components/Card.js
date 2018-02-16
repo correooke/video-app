a3fdf2793d2e6a28ccc5a4f2fc8aeb0e72a10cc8
HSNBDES33\eocariz <eocariz@hasar.com>
2018-02-16 11:55:37 -0300

    Estilo con StyleSheet

--- components/Card.js
+++ components/Card.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, 
+        Text, 
+        StyleSheet } from 'react-native';
 
 const Card = () => {
     // ejecución de función. 
 
     return (
-        <View>
-            <Text>
+        <View style={styles.container}>
+            <Text style={styles.title} >
                 CARD (EJEMPLO)!
             </Text>
         </View> 
     )
 };
 
+const styles = StyleSheet.create({
+    container: {
+        height: 200,
+        width: 300,
+        backgroundColor: '#F00',
+    },
+    title: {
+        fontSize: 30
+    }
+})
 export default Card;
