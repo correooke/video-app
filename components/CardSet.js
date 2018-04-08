63349d4dc92668044d2f097f3164fa419bf981be
HSNBDES33\eocariz <eocariz@hasar.com>
2018-04-08 12:59:48 -0300

    Utilizamos map para iterar items

--- components/CardSet.js
+++ components/CardSet.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+const Items = ['Item 1', 'Item 2', 'Item 3'];
+
+const ShowItem = (item) => (
+    <Text>{item}</Text>
+);
+
 const CardSet = () => (
     <View>
-        <Text>
-            Card Set
-        </Text>
+        {
+            // ES6 "función Map"
+            // Item1, Item2, Item3 
+            Items.map(ShowItem)
+        }
     </View>
 );
 
