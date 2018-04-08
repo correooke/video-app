c193329c17726aef9bbfd37afd73fa30ea86f644
HSNBDES33\eocariz <eocariz@hasar.com>
2018-04-08 18:36:40 -0300

    Damos estilo a CardSet

--- components/CardSet.js
+++ components/CardSet.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, Text, View, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 import Card from './Card';
 
 const CardSet = ({ items }) => (
     <ScrollView>
-        {
-            items.map(item => <Card 
-                                    key={item.title} 
-                                    {...item} >
-                            </Card>)
-        }
+        <View style={styles.container} >
+            {
+                items.map(item =>
+                                <View 
+                                    key={item.title}
+                                    style={styles.cardStyle}>
+                                    <Card 
+                                             
+                                            {...item} >
+                                    </Card>
+                                </View> 
+                            )                           
+            }
+        </View>
     </ScrollView>
 );
 
+const styles = StyleSheet.create(
+    {
+        container: {
+            padding: 20,
+            backgroundColor: '#E1E2E1',
+        },
+        cardStyle: {
+            marginBottom: 20,
+        }
+    }
+)
 CardSet.propTypes = {
     items: PropTypes.arrayOf(
         PropTypes.shape(
